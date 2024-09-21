63f91ccbffa99b78ee01cd587b2b519cfa2bc0b5
thiami <thiamibra1121@gmail.com>
2024-09-21 19:15:28 +0200

    bingocd ../..!

--- django-site/myproject/api/urls.py
+++ django-site/myproject/api/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import ListeActivites
 from .views import ActivitesFiltrer
 urlpatterns = [
-    path('activites/', ListeActivites.as_view(), name='api_liste_activites'),
-    path('activites/selection/', ActivitesFiltrer.as_view(), name='activites_filtrer'),
+    path('', ListeActivites.as_view(), name='api_liste_activites'),
+    path('filter/', ActivitesFiltrer.as_view(), name='activites_filtrer'),
 ]
